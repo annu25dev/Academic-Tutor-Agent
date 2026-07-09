4877bd03a15f437b38df5b48b7ecce5ee8b3a70d
Sanchita Pandey <sanchitap2006@gmail.com>
2026-07-09 10:02:23 +0530

    Added Backend folder with agent and task files

---
+++ Backend/quiz_agent.py
@@ -0,0 +1,23 @@
+from crewai import Agent
+from llm import llm  
+
+quiz_agent = Agent(
+    role="Quiz Agent",
+    
+    goal="""
+    Design highly dynamic, context_aware assessment materials_focusing
+    strictly on conceptual MCQs, fill-in-the-blank queries, and one-word
+    type questions tailored precisely to the student's level and background.
+    """,
+    
+    backstory="""
+    You are an academic evaluator responsible for creating quick-recall
+    quizzes (MCQs, blanks, and short answers) that verify basic comprehension
+    and core retention without requiring deep, long_form essay grading.
+    """,
+    
+    tools=[],
+    llm=llm,
+    verbose=True,
+    allow_delegation=False  
+)
